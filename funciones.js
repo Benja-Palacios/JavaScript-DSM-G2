5193f771a5c70985c860999ede763014c3aa0d47
Benjamin_Mendoza <benjijr96@gmail.com>
2023-01-19 14:18:36 -0600

    Tema de Funciones Terminado

--- funciones.js
+++ funciones.js
@@ -15,3 +15,92 @@ function saludo3(){
     }
 }
 console.log(saludo3()());
+
+function hello3(name) {
+    return 'hola ' + name;
+}
+
+console.log(hello3('Luis'));
+console.log(hello3());
+
+function agregar(x, y) {
+    return x + y;
+}
+
+console.log(agregar());
+console.log(agregar(5, 4));
+
+//Parametros por default
+
+function agragr2(x, y) {
+    if (y === undefined) {
+        y = 0;
+    } else if (x === undefined) {
+        x = 0;
+    } else if (x === undefined &&
+        y === undefined) {
+        return 0;
+    }
+
+    return x + y;
+
+}
+
+function agregrar3(x, y = 1, z = 2) {
+    return x + y * z;
+}
+
+console.log(agregrar3(5));
+
+function sumar() {
+    return 3 + 4;
+}
+
+//Arrow function
+const suma = () => 3 + 4;
+
+console.log(sumar());
+console.log(suma());
+
+function f1(x) {
+    return "Hola soy: " + x;
+}
+
+const f1a = x => "Hola soy: " + x;
+console.log(f1("Gallardo"));
+console.log(f1a("Mariano"));
+
+function factorial(n){
+    if(n==0 || n== 1){
+        return 1;
+    }else{
+        return n*factorial(n-1)
+    }
+}
+
+
+function factorial2(n){
+    i=1;
+    fact = 1;
+    while(i<=n){
+        fact*=i;
+        i++;
+    }
+    return fact;
+}
+//formar de flecha
+const f= n => {
+    i=1;
+    fact = 1;
+    while(i<=n){
+        fact*=i;
+        i++;
+    }
+    return fact;
+}
+const valor = numero => (numero % 2 == 0) ? 'Numero Par' : 'Numero Impar'
+
+console.log(factorial(3));
+console.log(factorial2(3));
+console.log(f(3));
+console.log(valor(9));

e55422c9e72e7af87e479931c9f6a088135bcc22
Benjamin Mendoza <benjijr96@gmail.com>
2023-01-30 13:16:44 -0600

    Commit de Promesas Terminado

--- promesas.js
+++ promesas.js
@@ -20,3 +20,106 @@ const usuarioAutenticado = new Promise (
 usuarioAutenticado
 .then((resultado)=>console.log(resultado))
 .catch((error)=>close.log(error))
+
+//Funcion declarada
+/**
+ * function cuadradoPromesa(value) {
+    if(typeof value !== "number"){
+        return Promise.reject(`Error, el valor ${value} no es un numero`)
+    }
+
+    return new Promise((resolve, reject) => {
+
+        setTimeout(() => {
+            resolve({
+                value: value,
+                result: Math.pow(value,2)
+            })
+        }, 3000);
+    })
+
+};
+
+
+cuadradoPromesa(2)
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        return cuadradoPromesa(3)
+    })
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        return cuadradoPromesa(4)
+    })
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        return cuadradoPromesa("x")
+    })
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        
+    })
+    .catch(err => console.log(err));
+
+ */
+    //funcion de flecha
+    
+    const cuadradoPromesa2 = (value) =>{
+        if(typeof value !== "number"){
+            return Promise.reject(`Error, el valor ${value} no es un numero`)
+        }
+    
+        return new Promise((resolve, reject) => {
+    
+            setTimeout(() => {
+                resolve({
+                    value: value,
+                    result: Math.pow(value,2)
+                })
+            }, 3000);
+        })
+    
+    }
+    
+    cuadradoPromesa2(2)
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        return cuadradoPromesa2(3)
+    })
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        return cuadradoPromesa2(4)
+    })
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        return cuadradoPromesa2("x")
+    })
+    .then((respuesta) => {
+        console.log("Inicia la promesa");
+        console.log(`Promesa: valor: ${respuesta.value}
+        cuadrado: ${respuesta.result}`
+        );
+        
+    })
+    .catch(err => console.log(err));
+    
